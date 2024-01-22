672f8137cefba37402764d10ea2dc330f322dcc2
Keanu Segers <keanusegers@hotmail.com>
2024-01-23 00:22:59 +0100

    add maze en player

---
+++ Millieu Mafia/Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft;
+    public bool timerOn = false;
+
+    public TMP_Text timerTxt;
+
+    void Start()
+    {
+        timerOn = true;
+    }
+
+    void Update()
+    {
+        if (timerOn)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer(timeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                timeLeft = 0;
+                timerOn = false;
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
